224d229ebcb01832045c24ee77b7e346f34edf6c
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-05-23 13:02:57 +0545

    Updated

--- src/numbergame/Test.java
+++ src/numbergame/Test.java
@@ -1,18 +1,36 @@
 package numbergame;
 
 import java.util.Random;
-//import java.util.Scanner;
+import java.util.Scanner;
+
 
 public class Test {
-
+	
+	static Random rd = new Random();
+	static int random = rd.nextInt(100)+1;
+	
 	public static void main(String[] args) {
-		Random rd = new Random();
 		
-		int random = rd.nextInt(100)+1;
 		System.out.println(random);
+
+		userchoice(random);
+		
 		}
 	
 	
+	static void userchoice(int rand) {
+		System.out.println("Guess a number between 1 to 100 ");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Your Number: ");
+		int guess = sc.nextInt();
+		
+		if (guess == rand) {
+			System.out.println("Congratulations! Its a match.");
+		}
+		else if (guess < rand) {
+			System.out.println("Try Guessing Higher Number. ");
+		}
+	}
 	
 	}
 
