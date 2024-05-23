e095dc050ff148ada981da17a97fac366495398b
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-05-23 12:34:08 +0545

    Random Number

--- src/numbergame/Test.java
+++ src/numbergame/Test.java
@@ -1,8 +1,18 @@
 package numbergame;
 
+import java.util.Random;
+//import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! ");
+		Random rd = new Random();
+		
+		int random = rd.nextInt(100)+1;
+		System.out.println(random);
+		}
+	
+	
+	
 	}
-}
+
